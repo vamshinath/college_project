4276af1a68b3c2f69212e27f07d06799356e62c3
vamshinath <vamshinath.m8@gmail.com>
2018-01-06 23:42:33 +0530

    current Time added

--- index.php
+++ index.php
@@ -35,13 +35,34 @@
 		</style>
 
 
+<script>
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+</script>
+
+
+
+
 	</head>
 
 
-<body>
+<body onload="startTime()">
 	<center>
 	<h2>CVR E-Bins</h2>
-	<marquee>Welcome</marquee></br></br>
+	<marquee id="txt">Welcome</marquee></br></br>
 	
 	<!--<div class="avatar-container p-<?php
 		$rs=mysql_query('select *from vamshi_team1db.table1 ORDER BY date DESC');
